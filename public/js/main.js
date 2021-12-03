ddb00bf42f009b9a9d958816abb93612e3f3cc6c
ayilmz <ayilmaz@ebay.com>
2021-12-03 11:39:40 +0300

    add slider

--- public/js/main.js
+++ public/js/main.js
@@ -37,4 +37,36 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         }
     }
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 8,
+        spaceBetween: 20,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 20,
+            },
+            1280: {
+                slidesPerView: 5,
+                spaceBetween: 20,
+            },
+            1440: {
+                slidesPerView: 8,
+                spaceBetween: 20,
+            },
+        }
+    });
 });
